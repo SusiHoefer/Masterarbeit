24c5575af51e138754322cc7820851a910a69bd3
SusiHoefer <39659956+SusiHoefer@users.noreply.github.com>
2026-04-14 12:18:12 +0200

    formatiert

--- API mit HTML Scraping.py
+++ API mit HTML Scraping.py
@@ -1,5 +1,6 @@
 import requests, json
 from bs4 import BeautifulSoup
+import re
 
 
 wort = input("Bitte gib ein Wort ein:")
@@ -9,13 +10,19 @@ url2 = f"https://www.dwds.de/wb/{wort}"
 response = requests.get(url, params={"q": wort})
 responsehtml = requests.get(url2)
 soup = BeautifulSoup(responsehtml.text,"html.parser")
-suppe = soup.find("div", class_="bedeutungsuebersicht")
+suppe = soup.find("div", class_=["bedeutungsuebersicht", "dwdswb-lesarten"])
 suppe = suppe.get_text() if suppe else "Keine Bedeutungsübersicht gefunden."
 
 
 if response.status_code == 200:
     data = response.json()
-    print(json.dumps(data, indent=2, ensure_ascii=False),suppe)
+    api= (json.dumps(data, indent=2, ensure_ascii=False)) #
+
+    formatted = re.sub(r'(\d+\.)', r'\n\1', suppe)
+    print(api)
+    print(formatted.strip())
+    
+
     
 else:
     print("Fehler:", response.status_code)
